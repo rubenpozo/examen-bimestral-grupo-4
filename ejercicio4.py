1e7da17f282626034937c491d5b7f418f6dc58f0
victoralonsopacha <victoralonsopacha@outlook.es>
2016-06-09 09:42:26 -0500

    traductor final
    inclusion del  guardado en el archivo de texto

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,20 +2,27 @@ from textblob import TextBlob
 
 def traduccion(text):
     traduc=TextBlob(text)
-    print (traduc.translate(to="en"))
+    return traduc.translate(to="en")
+
 
 frase=str(input("ingrese la frase a traducir\n"))
 print("tu frase es:")
 print(frase)
 print("tu traduccion es: ")  
-traduccion(frase)
+print(traduccion(frase))
 
 def creartxt():
-	archi=open('datos.txt', 'w')
+	archi=open('traducido.txt', 'w')
 	archi.close()
 
+texto= traduccion(frase)
+
 def grabartxt():
-	archi=open('datos.txt', 'a')
-	archi.write('linea1 \n')
+	archi=open('traducido.txt', 'a')
+	archi.write(str(texto))
 	archi.close()
+
+creartxt()
+grabartxt()
+
 
