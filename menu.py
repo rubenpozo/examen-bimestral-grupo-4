4169ef29e3051f5c5511198e6f8cb4179ec0934d
Mario Nuñez <pepeseya@hotmail.com>
2016-06-09 09:57:39 -0500

    FALTA
    falta funciones

--- menu.py
+++ menu.py
@@ -1,3 +1,22 @@
+def creartxt(list):
+    archi=open("txtprimos.txt","a")
+    archi.write(list)
+    archi.write("\n")
+    archi.close()
+    pass
+def ejercicio1():
+	n=int(input("ingrese un nuemero hasta el cual desea desplegar numeros primos\n"))
+	listap=[]
+	for i in range(2,n+1):
+		for x in range(2,i):
+			if (i%x==0 ):
+				break
+		else:
+		creartxt(str(i))
+		listap.append(str(i))
+	print(listap)
+	pass
+
 def menu():
 	print('------menu-----')
 	print('1.-Secuencia de numeros primos')
@@ -13,7 +32,7 @@ while(continuar==1):
 	menu()	
 	
 	if(opcion==1):
-
+		ejercicio(1)
 	if(opcion==2):
 
 	if(opcion==3):
