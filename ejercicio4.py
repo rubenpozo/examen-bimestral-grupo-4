6672c9c015c396696566f3aea6405b665a34abc1
rubenpozo <ruben_oso28@hotmail.com>
2016-06-09 09:47:34 -0500

    ejercicio completo
    no modificar

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,28 +1,17 @@
 from textblob import TextBlob
 
-def traduccion(text):
-    traduc=TextBlob(text)
-    return traduc.translate(to="en")
+def creartxt(list):
+    archi=open("textoeje4.txt","a")
+    archi.write(list)
+    archi.write("\n")
+    archi.close()
+    pass
 
-
-frase=str(input("ingrese la frase a traducir\n"))
-print("tu frase es:")
+frase=str(input("ingrese la palabra a traducir\n"))
+print("tu palabra es:")
 print(frase)
-print("tu traduccion es: ")  
-print(traduccion(frase))
-
-def creartxt():
-	archi=open('traducido.txt', 'w')
-	archi.close()
-
-texto= traduccion(frase)
-
-def grabartxt():
-	archi=open('traducido.txt', 'a')
-	archi.write(str(texto))
-	archi.close()
-
-creartxt()
-grabartxt()
-
-
+print("tu traduccion es: ")
+traduc=TextBlob(frase)
+traduc.translate(to="en")
+print (traduc.translate(to="en"))
+creartxt(frase)
