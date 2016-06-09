0a6c93c39586b8458814f2ce6fc7c391e702b902
rubenpozo <ruben_oso28@hotmail.com>
2016-06-09 07:35:42 -0500

    calculo de primos completo
    falta que guarde en un txt

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,8 @@
-
-
-n=int(input("ingrese un nuemero hasta el cual desea desplegar numeros primos"))
-for x in xrange(1,10):
-	pass
+n=int(input("ingrese un nuemero hasta el cual desea desplegar numeros primos\n"))
+for i in range(2,n+1):
+	for x in range(2,i):
+		if (i%x==0 ):
+			break
+	else:
+		print(i)
+pass
