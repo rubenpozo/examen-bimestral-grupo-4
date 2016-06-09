6b05e25ced0107d60ef551c014e7607982701fa6
rubenpozo <ruben_oso28@hotmail.com>
2016-06-09 07:45:40 -0500

    ejercicio 1 completo
    no modificar

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,18 @@
+def creartxt(list):
+    archi=open("txtprimos.txt","a")
+    archi.write(list)
+    archi.write("\n")
+    archi.close()
+    pass
+
 n=int(input("ingrese un nuemero hasta el cual desea desplegar numeros primos\n"))
+listap=[]
 for i in range(2,n+1):
 	for x in range(2,i):
 		if (i%x==0 ):
 			break
 	else:
-		print(i)
+		creartxt(str(i))
+		listap.append(str(i))
+print(listap)
 pass
