34012d8ff33e7867dd8fb3fac71e0fe839ad47f0
rubenpozo <ruben_oso28@hotmail.com>
2016-06-09 08:36:27 -0500

    ejercicio completo
    no modificar

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-x=int(input("ingrese un numero:"))
-y=int(input("ingrese un numero:"))
-def multiplos(x,y):
-	if x>y:
-		print("no hay numeros")
-	else:
-		cadena=[]
-		n=0
-		for i in range(x,y,x):
-			n+=1
-			cadena.append(i)
-
-		cadena=str(cadena).replace('[',"")
-		cadena=str(cadena).replace(']',"")
-		print("hay n numeros"+str(x))
-		print(cadena)
-	
-multiplos(x,y)
-=======
 def creartxt(list):
     archi=open("txteje2.txt","a")
     archi.write(list)
@@ -39,14 +19,5 @@ else:
 
 cadena=str(cadena).replace('[',"")
 cadena=str(cadena).replace(']',"")
-print("hay n numeros"+str(x))
 print(cadena)
 pass
-		
-
->>>>>>> origin/master
-
-
-
-
-
